49a16362de426ea6b89e37bf8ae13dfb7b7f921f
proto <proto@users.noreply.github.com>
2023-07-06 14:12:44 +0200

    every hour / no temp/lux

--- main.ts
+++ main.ts
@@ -1,13 +1,11 @@
 let moisture = 0
-power.fullPowerEvery(60000, function () {
+power.fullPowerEvery(3600000, function () {
     radio.setGroup(77)
     radio.setTransmitSerialNumber(true)
     led.setBrightness(100)
     music.setVolume(160)
     moisture = pins.analogReadPin(AnalogPin.P1)
     if (true) {
-        radio.sendValue(control.deviceName(), input.temperature())
-        radio.sendValue(control.deviceName(), input.lightLevel())
         radio.sendValue(control.deviceName(), moisture)
     }
     if (moisture <= 400) {
