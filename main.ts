4bbfc649c81c8eea7a42dd23808ea1894eb8981a
proto <proto@users.noreply.github.com>
2023-07-06 13:26:18 +0200

    unlog... no buttons, send only

--- main.ts
+++ main.ts
@@ -1,62 +1,22 @@
 let moisture = 0
-datalogger.onLogFull(function () {
+power.fullPowerEvery(60000, function () {
     radio.setGroup(77)
     radio.setTransmitSerialNumber(true)
-    radio.sendString("" + control.deviceName() + "\"LOG FULL\"")
-    power.lowPowerRequest()
-})
-input.onButtonPressed(Button.A, function () {
-    basic.showString("A:" + ("" + moisture))
-    basic.clearScreen()
-    power.lowPowerRequest()
-})
-input.onButtonPressed(Button.AB, function () {
-    power.lowPowerEnable(LowPowerEnable.Prevent)
-    basic.showString("LOG RESET!")
-    datalogger.deleteLog(datalogger.DeleteType.Full)
-    control.reset()
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showString("L:" + ("" + input.lightLevel()))
-    basic.showString("T:" + ("" + input.temperature()))
-    basic.clearScreen()
-    power.lowPowerRequest()
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showString(control.deviceName())
-    basic.clearScreen()
-    power.lowPowerRequest()
-})
-power.fullPowerEvery(60000, function () {
-    if (input.lightLevel() > 50) {
-        radio.setGroup(77)
-        radio.setTransmitSerialNumber(true)
-        led.setBrightness(99)
-        datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
-        datalogger.setColumnTitles(
-        "temp",
-        "lum",
-        "h2o"
-        )
-        moisture = pins.analogReadPin(AnalogPin.P1)
-        if (true) {
-            radio.sendValue(control.deviceName(), input.temperature())
-            radio.sendValue(control.deviceName(), input.lightLevel())
-        }
-        if (moisture <= 400) {
-            radio.sendValue(control.deviceName(), moisture)
-            basic.showIcon(IconNames.Sad)
-            basic.pause(500)
-            basic.clearScreen()
+    led.setBrightness(100)
+    music.setVolume(160)
+    moisture = pins.analogReadPin(AnalogPin.P1)
+    if (true) {
+        radio.sendValue(control.deviceName(), input.temperature())
+        radio.sendValue(control.deviceName(), input.lightLevel())
+        radio.sendValue(control.deviceName(), moisture)
+    }
+    if (moisture <= 400) {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(1000)
+        basic.clearScreen()
+        if (input.lightLevel() > 0) {
             music.play(music.builtinPlayableSoundEffect(soundExpression.yawn), music.PlaybackMode.InBackground)
-        } else {
-            radio.sendValue(control.deviceName(), moisture)
         }
-        datalogger.log(
-        datalogger.createCV("temp", input.temperature()),
-        datalogger.createCV("lum", input.lightLevel()),
-        datalogger.createCV("h2o", moisture)
-        )
     }
     power.lowPowerRequest()
 })
